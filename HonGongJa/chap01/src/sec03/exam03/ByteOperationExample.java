849f646d5b18cefd91718751ca49799c4c88ef59
Seo JeongSeop <seo92js@gmail.com>
2022-10-13 18:12:51 +0900

    2022.10.13

---
+++ HonGongJa/chap01/src/sec03/exam03/ByteOperationExample.java
@@ -0,0 +1,18 @@
+package sec03.exam03;
+
+public class ByteOperationExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte result1 = 10 + 20;
+		System.out.println(result1);
+		
+		byte x = 10;
+		byte y = 20;
+		int result2 = x + y;
+		//byte result3 = x + y; 정수타입 변수가 피연산자로 사용되면 int로 자동 타입 변환
+		System.out.println(result2);
+	}
+
+}
