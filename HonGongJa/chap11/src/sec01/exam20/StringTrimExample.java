c500eb1385733da1e08c3d0c4c6cb126d7033f50
Seo JeongSeop <seo92js@gmail.com>
2022-11-15 01:25:26 +0900

    2022.11.14

---
+++ HonGongJa/chap11/src/sec01/exam20/StringTrimExample.java
@@ -0,0 +1,16 @@
+package sec01.exam20;
+
+public class StringTrimExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String tel1 = "   02";
+		String tel2 = "123   ";
+		String tel3 = "    1234   ";
+		
+		String tel = tel1.trim() + tel2.trim() + tel3.trim();
+		System.out.println(tel);
+	}
+
+}
