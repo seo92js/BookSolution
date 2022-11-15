7407235294775fa2af11fda2ee3e200ee4460d63
Seo JeongSeop <seo92js@gmail.com>
2022-11-16 02:19:06 +0900

    2022.11.15

---
+++ HonGongJa/chap11/src/sec01/exam27/MathRandomExample.java
@@ -0,0 +1,12 @@
+package sec01.exam27;
+
+public class MathRandomExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num = (int)(Math.random() * 6) + 1;
+		System.out.println("주사위 눈 : " + num);
+	}
+
+}
