0933fa247b78e99e97a1a054abd71bb2d504b88a
Seo JeongSeop <seo92js@gmail.com>
2022-11-21 01:26:42 +0900

    2022.11.20

---
+++ HonGongJa/chap12/src/sec02/exam05/DaemonExample.java
@@ -0,0 +1,21 @@
+package sec02.exam05;
+
+public class DaemonExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AutoSaveThread autoSaveThread = new AutoSaveThread();
+		autoSaveThread.setDaemon(true);
+		autoSaveThread.start();
+		
+	
+		try {
+			Thread.sleep(3000);
+		}catch(InterruptedException e) {
+			
+		}
+		
+		System.out.println("메인 스레드 종료");
+	}
+}
---
+++ HonGongJa/chap13/src/sec01/exam03/LinkedListExample.java
@@ -0,0 +1,37 @@
+package sec01.exam03;
+
+import java.util.*;
+
+public class LinkedListExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<String> list1 = new ArrayList<String>();
+		List<String> list2 = new LinkedList<String>();
+		
+		long startTime;
+		long endTime;
+		
+		startTime = System.nanoTime();
+		
+		for(int i = 0; i< 10000; i++) {
+			list1.add(0, String.valueOf(i));
+		}
+		
+		endTime = System.nanoTime();
+		
+		System.out.println("ArrayList : " + (endTime - startTime) + " ns");
+		
+		startTime = System.nanoTime();
+		
+		for(int i = 0; i< 10000; i++) {
+			list2.add(0, String.valueOf(i));
+		}
+		
+		endTime = System.nanoTime();
+		
+		System.out.println("ArrayList : " + (endTime - startTime) + " ns");
+	}
+
+}
