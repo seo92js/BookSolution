e21228add1c204d96ccd320d535d884ad8852831
Seo JeongSeop <seo92js@gmail.com>
2022-11-25 01:48:48 +0900

    2022.11.24

---
+++ HonGongJa/chap14/src/sec03/exam02/ProductStorageExample.java
@@ -0,0 +1,12 @@
+package sec03.exam02;
+
+public class ProductStorageExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ProductStorage productStorage = new ProductStorage();
+		productStorage.showMenu();
+	}
+
+}
